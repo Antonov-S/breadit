e3a1de81196c38d2a2c613869102ca701d3c8213
Antonov-S <anto1nov1@gmail.com>
2023-07-04 22:24:31 +0300

    Finishing creating and voting for comments (bug in voting for commnents)

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,4 +1,6 @@
+import CommentsSection from "@/components/CommentsSection";
 import EditorOutput from "@/components/EditorOutput";
+import PostVoteServer from "@/components/post-vote/PostVoteServer";
 import { buttonVariants } from "@/components/ui/Button";
 import { db } from "@/lib/db";
 import { redis } from "@/lib/redis";
@@ -8,9 +10,8 @@ import { Post, User, Vote } from "@prisma/client";
 import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
-import PostVoteServer from "@/components/post-vote/PostVoteServer";
 
-interface PageProps {
+interface SubRedditPostPageProps {
   params: {
     postId: string;
   };
@@ -19,7 +20,7 @@ interface PageProps {
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
-const page = async ({ params }: PageProps) => {
+const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
   const cachedPost = (await redis.hgetall(`post:${params.postId}`)) as CachedPost;
 
   let post: (Post & { votes: Vote[]; author: User }) | null = null;
@@ -65,32 +66,35 @@ const page = async ({ params }: PageProps) => {
           <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">{post?.title ?? cachedPost.title}</h1>
 
           <EditorOutput content={post?.content ?? cachedPost.content} />
-          <Suspense fallback={<Loader2 className="h-5 w-5 animate-spin text-zinc-500" />}>{/* <CommentsSection postId={post?.id ?? cachedPost.id} /> */}</Suspense>
+          <Suspense fallback={<Loader2 className="h-5 w-5 animate-spin text-zinc-500" />}>
+            {/* @ts-expect-error Server Component */}
+            <CommentsSection postId={post?.id ?? cachedPost.id} />
+          </Suspense>
         </div>
       </div>
     </div>
   );
-
-  function PostVoteShell() {
-    return (
-      <div className="flex items-center flex-col pr-6 w-20">
-        {/* upvote */}
-        <div className={buttonVariants({ variant: "ghost" })}>
-          <ArrowBigUp className="h-5 w-5 text-zinc-700" />
-        </div>
-
-        {/* score */}
-        <div className="text-center py-2 font-medium text-sm text-zinc-900">
-          <Loader2 className="h-3 w-3 animate-spin" />
-        </div>
-
-        {/* downvote */}
-        <div className={buttonVariants({ variant: "ghost" })}>
-          <ArrowBigDown className="h-5 w-5 text-zinc-700" />
-        </div>
-      </div>
-    );
-  }
 };
 
-export default page;
+function PostVoteShell() {
+  return (
+    <div className="flex items-center flex-col pr-6 w-20">
+      {/* upvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      </div>
+
+      {/* score */}
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h-3 w-3 animate-spin" />
+      </div>
+
+      {/* downvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      </div>
+    </div>
+  );
+}
+
+export default SubRedditPostPage;
